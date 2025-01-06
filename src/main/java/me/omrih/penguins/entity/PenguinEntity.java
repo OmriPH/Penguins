1cbf2fdd75c437ee8389391cc9f94653e54ff486
Omri H <omri.hermon@gmail.com>
2025-01-06 18:21:06 +0200

    start doing riding stuff

--- src/main/java/me/omrih/penguins/entity/PenguinEntity.java
+++ src/main/java/me/omrih/penguins/entity/PenguinEntity.java
@@ -1,11 +1,14 @@
 package me.omrih.penguins.entity;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.mob.PathAwareEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
@@ -26,7 +29,22 @@ public class PenguinEntity extends PathAwareEntity implements GeoEntity {
     }
 
     @Override
+    public LivingEntity getControllingPassenger() {
+        return this.getFirstPassenger() instanceof PlayerEntity playerEntity ? playerEntity : null;
+    }
+
+    @Override
+    protected void tickControlled(PlayerEntity controllingPlayer, Vec3d movementInput) {
+        super.tickControlled(controllingPlayer, movementInput);
+        Vec2f vec2f = new Vec2f(controllingPlayer.getPitch() * 0.5F, controllingPlayer.getYaw());
+        this.setRotation(vec2f.y, vec2f.x);
+        this.prevYaw = this.bodyYaw = this.headYaw = this.getYaw();
+    }
+
+    @Override
     public ActionResult interactMob(PlayerEntity player, Hand hand) {
+        player.setYaw(this.getYaw());
+        player.setPitch(this.getPitch());
         player.startRiding(this);
         return ActionResult.SUCCESS;
     }
