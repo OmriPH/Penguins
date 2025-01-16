16c65eff08f024f0e9dac55a9e5d35b33016e187
Omri <omri.hermon@gmail.com>
2025-01-16 19:29:13 +0200

    epic weird multiversion shit

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,25 @@ plugins {
     id("fabric-loom") version "1.9-SNAPSHOT"
 }
 
-version = project.property("mod_version") as String
-group = project.property("maven_group") as String
-
-base {
-    archivesName.set(project.property("archives_base_name") as String)
+class ModData {
+    val id = property("mod.id").toString()
+    val name = property("mod.name").toString()
+    val version = property("mod.version").toString()
+    val group = property("mod.group").toString()
 }
+
+class ModDependencies {
+    operator fun get(name: String) = property("deps.$name").toString()
+}
+
+val mod = ModData()
+val deps = ModDependencies()
+val mcVersion = stonecutter.current.version
+val mcDep = property("mod.mc_dep").toString()
+
+version = "${mod.version}+$mcVersion"
+group = mod.group
+base { archivesName.set(mod.id) }
 
 repositories {
     maven {
@@ -24,37 +37,51 @@ repositories {
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
-    mappings(loom.layered() {
+    minecraft("com.mojang:minecraft:$mcVersion")
+    mappings(loom.layered {
         officialMojangMappings()
-        parchment("org.parchmentmc.data:parchment-${project.property("minecraft_version")}:${project.property("parchment_version")}@zip")
+        parchment("org.parchmentmc.data:parchment-$mcVersion:${deps["parchment_version"]}@zip")
     })
-    modImplementation("net.fabricmc:fabric-loader:${project.property("loader_version")}")
-    modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_version")}")
+    modImplementation("net.fabricmc:fabric-loader:${deps["fabric_loader"]}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${deps["fabric_api"]}")
 
-    modImplementation("software.bernie.geckolib:geckolib-fabric-${project.property("minecraft_version")}:${project.property("geckolib_version")}")
+    modImplementation("software.bernie.geckolib:geckolib-fabric-$mcVersion:${deps["geckolib_version"]}")
+}
+
+loom.runConfigs {
+    all {
+        ideConfigGenerated(true)
+        runDir = "../../run"
+    }
 }
 
 tasks.processResources {
-    inputs.property("version", project.version)
-    inputs.property("minecraft_version", project.property("minecraft_version"))
-    inputs.property("loader_version", project.property("loader_version"))
-    filteringCharset = "UTF-8"
+    inputs.property("id", mod.id)
+    inputs.property("name", mod.name)
+    inputs.property("version", mod.version)
+    inputs.property("mcdep", mcDep)
+    inputs.property("fabric_loader", deps["fabric_loader"])
 
-    filesMatching("fabric.mod.json") {
-        expand("version" to project.version,
-            "minecraft_version" to project.property("minecraft_version"),
-            "loader_version" to project.property("loader_version"),
-            "loader_version" to project.property("loader_version"))
-    }
+    val map = mapOf(
+        "id" to mod.id,
+        "name" to mod.name,
+        "version" to mod.version,
+        "mcdep" to mcDep,
+        "fabric_loader" to deps["fabric_loader"]
+    )
+
+    filesMatching("fabric.mod.json") { expand(map) }
 }
 
 java {
-    toolchain.languageVersion = JavaLanguageVersion.of(21)
+    val java = if (stonecutter.eval(mcVersion, ">=1.20.6")) JavaVersion.VERSION_21 else JavaVersion.VERSION_17
+    targetCompatibility = java
+    sourceCompatibility = java
 }
 
-tasks.jar {
-    from("LICENSE") {
-        rename { "${it}_${project.base.archivesName}" }
-    }
+tasks.register<Copy>("buildAndCollect") {
+    group = "build"
+    from(tasks.remapJar.get().archiveFile)
+    into(rootProject.layout.buildDirectory.file("libs/${mod.version}"))
+    dependsOn("build")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,3 +7,17 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
+plugins {
+    id("dev.kikugie.stonecutter") version "0.5.1"
+}
+
+stonecutter {
+    kotlinController = true
+    centralScript = "build.gradle.kts"
+
+    create(rootProject) {
+        versions("1.21.1")
+        vcsVersion = "1.21.1"
+    }
+}
